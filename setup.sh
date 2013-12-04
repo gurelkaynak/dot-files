90a203e0bb1f69102661969864d5db3757abfdee
Gürel Kaynak <gurelkaynak@gmail.com>
2013-12-04 21:01:34 +0200

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+ln -s /home/gurel/dot-files/.vim /home/gurel/
+ln -s /home/gurel/dot-files/.vimrc /home/gurel/
+ln -s /home/gurel/dot-files/.gitconfig /home/gurel/
+ln -s /home/gurel/dot-files/.bashrc /home/gurel/
+ln -s /home/gurel/dot-files/.bash_aliases /home/gurel/
