59d7e4b7f31a85772a638b2cff6ac4778734556d
Gürel Kaynak <gurelkaynak@gmail.com>
2013-12-19 15:17:31 +0200

    setup

--- setup.sh
+++ setup.sh
@@ -3,3 +3,11 @@ ln -s /home/gurel/dot-files/.vimrc /home/gurel/
 ln -s /home/gurel/dot-files/.gitconfig /home/gurel/
 ln -s /home/gurel/dot-files/.bashrc /home/gurel/
 ln -s /home/gurel/dot-files/.bash_aliases /home/gurel/
+
+
+sudo su
+
+echo "172.16.0.231 asterix" >> /etc/hosts
+echo "172.16.0.238 apache" >> /etc/hosts
+echo "172.16.0.13 amiga" >> /etc/hosts
+echo "172.16.1.231 bilgitest" >> /etc/hosts
