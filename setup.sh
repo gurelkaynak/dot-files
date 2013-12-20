bb021be343877718d0f661b1455f5c3494f23f6c
Gürel Kaynak <gurelkaynak@gmail.com>
2013-12-20 11:01:31 +0200

    setup

--- setup.sh
+++ setup.sh
@@ -4,10 +4,6 @@ ln -s /home/gurel/dot-files/.gitconfig /home/gurel/
 ln -s /home/gurel/dot-files/.bashrc /home/gurel/
 ln -s /home/gurel/dot-files/.bash_aliases /home/gurel/
 
-
-sudo su
-
-echo "172.16.0.231 asterix" >> /etc/hosts
-echo "172.16.0.238 apache" >> /etc/hosts
-echo "172.16.0.13 amiga" >> /etc/hosts
-echo "172.16.1.231 bilgitest" >> /etc/hosts
+echo "adding hosts"
+curdir=`pwd`
+su - -c "cd $curdir && cat hosts >> /etc/hosts"
